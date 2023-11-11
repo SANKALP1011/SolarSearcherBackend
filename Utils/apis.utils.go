7756ede106c121e326d85ec78744b6eb6cb61611
SANKALP PANDEY <84143581+SANKALP1011@users.noreply.github.com>
2023-11-11 23:29:03 +0530

    Code Cleaning: Removed irelevant comments

--- Utils/apis.utils.go
+++ Utils/apis.utils.go
@@ -1,3 +1,4 @@
 package Utils
 
 const ISSLiveLocationURL = "https://api.wheretheiss.at/v1/satellites/25544"
+const MarsRoverApi = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY"
